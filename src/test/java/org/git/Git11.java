a248c45e871cd183f9aa321916ac231c68a8c8ee
vasanth8888 <vasanthveeda09@gmail.com>
2022-04-04 17:28:40 +0530

    add github11

---
+++ src/test/java/org/git/Git11.java
@@ -0,0 +1,23 @@
+package org.git;
+
+public class Git11 {
+	
+	
+	
+	private void hello() {
+		// TODO Auto-generated method stub
+  System.out.println(1);
+	}
+	
+	
+	private void hello1() {
+		// TODO Auto-generated method stub
+		 System.out.println(2);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
